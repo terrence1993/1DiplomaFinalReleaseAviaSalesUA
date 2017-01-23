b860f3031e6bee73722507d047075d1d45626325
terrence1993 <aezakmi12>
2017-01-24 00:34:17 +0200

    pom

--- src/main/java/com/gmail/zork93/SecurityConfig.java
+++ src/main/java/com/gmail/zork93/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/").hasAnyRole("USER", "ADMIN")
                     .antMatchers("/admin").hasRole("ADMIN")
