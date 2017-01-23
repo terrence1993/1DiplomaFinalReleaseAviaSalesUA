37a4976da114c632a3642ba4f8239abbeb5313f4
terrence1993 <aezakmi12>
2017-01-23 23:21:32 +0200

    pom

--- src/main/java/com/gmail/zork93/SecurityConfig.java
+++ src/main/java/com/gmail/zork93/SecurityConfig.java
@@ -27,8 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .requiresChannel().anyRequest().requiresSecure()
-                .and()
+
                 .authorizeRequests()
                     .antMatchers("/").hasAnyRole("USER", "ADMIN")
                     .antMatchers("/admin").hasRole("ADMIN")
